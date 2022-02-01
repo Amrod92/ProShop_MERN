240caacfa0b81863a6f38e9fe3c9994169be5c30
Amrod92 <manlio92--@live.it>
2022-02-01 17:59:02 +0000

    MongoDB implementation

---
+++ backend/models/productModel.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const reviewScherma = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+const productSchema = mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+    name: { type: String, required: true },
+    image: { type: String, required: true, unique: true },
+    brand: { type: String, required: true },
+    category: { type: String, required: true },
+    reviews: [reviewScherma],
+    description: { type: String, required: true },
+    rating: { type: Number, required: true, default: 0 },
+    numReviews: { type: Number, required: true, default: 0 },
+    price: { type: Number, required: true, default: 0 },
+    countInStock: { type: Number, required: true, default: 0 },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
 import products from './data/products.js';
 
+import productRoutes from './routes/productRoutes.js';
+
 dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
+
+app.use('/api/products', productRoutes);
 
 app.get('/api/products', (req, res) => {
   res.json(products);
@@ -19,9 +28,15 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
+app.use(notFound);
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 8001;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
 );
